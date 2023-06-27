804330db110a0216e9854f24a9fc8872a59978e8
Ellis Thompson <>
2023-06-27 10:26:38 +0100

    Added comments to the environment files

--- src/environment/aircraft.py
+++ src/environment/aircraft.py
@@ -1,10 +1,11 @@
 
 import numpy as np
 
-from numba import njit
-
 class Aircraft:
+    """Thre aircraft class holds information on the state of the aircraft.
+    """
     def __init__(self, max_vel=25, max_accel = 14):
+        # Initilise the values to record
         self.positions = []
         self.velocities = []
         self.accelarations = []
@@ -13,19 +14,24 @@ class Aircraft:
         self.flight_times = []
         self.max_flight_times = []
         
-        self.n_ac = 0
+        self.n_ac = 0 # The number of aircraft in the sim
         
         self.max_vel = max_vel
         self.max_accel = max_accel
         
-        self.updates = 0
+        self.updates = 0 # Howmany updates have occured
     
     def add_ac(self, bounds:(float,float), pos:(float,float)=None, vel:(float,float)=None, accel:(float,float)=None, flight_time_bounds:(float,float)=(10,30)):
+        """Add an aircraft to the environment with either predefined vars or randomly generated is None.
+        """
+        
+        # Check if vars are pre existing
         pos = np.random.uniform((0,0), bounds, 2) if pos is None else np.asarray(pos)
         vel = np.random.uniform(-self.max_vel, self.max_vel, 2) if vel is None else np.asarray(vel)
         accel = np.random.uniform(-self.max_accel, self.max_accel,2) if accel is None else np.asarray(accel)
         max_flight_time = np.random.uniform(*flight_time_bounds)
 
+        # Append aircraft to the state storers
         if self.n_ac == 0:
             self.fresh_aircraft(pos, vel, accel,max_flight_time)
         else:
@@ -33,6 +39,8 @@ class Aircraft:
         self.n_ac+=1
 
     def append_aircraft(self, pos, vel, accel, max_flight_time):
+        """This function appends the aircraft to already occupied arrays
+        """
         self.positions = np.append(self.positions, [pos], axis=0)
         self.velocities = np.append(self.velocities, [vel], axis=0)
         self.accelarations = np.append(self.accelarations,[accel],axis=0)
@@ -42,6 +50,8 @@ class Aircraft:
         self.max_flight_times = np.append(self.max_flight_times, max_flight_time)
 
     def fresh_aircraft(self, pos, vel, accel,max_flight_time):
+        """This function instantiates new state storers and then appends an aircraft
+        """
         self.positions = np.array([pos])
         self.velocities = np.array([vel])
         self.accelarations = np.array([accel])
@@ -51,6 +61,8 @@ class Aircraft:
         self.max_flight_times = np.array([max_flight_time])
         
     def update(self, ts, bounds):
+        """Update the environment
+        """
         self.flight_times += ts # Update how long the aircraft have been in the air for
         
         # Get the list of active and inactive aircraft
@@ -61,24 +73,29 @@ class Aircraft:
         # Update the status of inactive aircraft
         self.active[inactive_idxs] = False
         
+        # Update the positions and headings
         self.positions[active_idxs] += self.velocities[active_idxs]*ts
         self.headings[active_idxs] = np.rad2deg(np.arctan2(self.velocities[[active_idxs],1], self.velocities[[active_idxs],0]))
-    
+
+        # Update the velocity and accel randomly
         self.velocities[active_idxs] = np.clip(self.velocities[active_idxs] + (self.accelarations[active_idxs]*ts), -self.max_vel, self.max_vel)
         self.velocities[inactive_idxs,:] = np.array([0.,0.])
         self.accelarations[inactive_idxs,:] = np.array([0.,0.])
         
         self.updates += 1
 
+        # Set the new accelerations
         if self.updates % 10 == 0:
             idxs = np.where(np.random.rand(len(active_idxs)) > 0.4)[0]
             accel_changes = np.random.uniform(-self.max_accel, self.max_accel, size = (idxs.shape[0],2))
             
             self.accelarations[active_idxs[idxs]] = np.clip(self.accelarations[active_idxs[idxs]]+accel_changes, -self.max_accel, self.max_accel)
         
-        self.validate_ac(bounds, active_idxs)
+        self.validate_ac(bounds, active_idxs) # Make sure aircraft dont leave the environment
     
     def validate_ac(self, bounds, active_idxs):
+        """Validate the states of the aircraft
+        """
         for i in range(2):
             result = active_idxs[np.where(self.positions[active_idxs, i] <= 0)[0]]
 
--- src/environment/main.py
+++ src/environment/main.py
@@ -9,76 +9,109 @@ from .towers import Tower, Tower_List, ring_to
 
 
 class State:
-    def __init__(self, bounds:(float,float), sim_t:float = 0.0):
+    """This holds aircraft state information as well as the sim time
+    """
+    def __init__(self, bounds:(float,float), sim_t:float = 0.0, N:int = 15):
         self.sim_t = sim_t
         
+        self.bounds = bounds # Physical bounds for aircraft to operate in
+        self.aircraft=Aircraft() # Instansiate the aircraft object
         
-        self.bounds = bounds
-        self.aircraft=Aircraft()
+        self.state_log = [] # This will be used to store states of aircraft
         
-        self.state_log = []
-        
-        for _ in range(50):
+        for _ in range(N): # Add N aircraft
             self.aircraft.add_ac(self.bounds)
             
-        self.log()
+        self.log() # Begin the logging
         
     def update(self, t_step:float):
-        self.sim_t += t_step*1000
+        """Update the aircraft states with a timestep of t seconds
+
+        Args:
+            t_step (float): The time step in seconds
+        """
+        self.sim_t += t_step*1000 # Convert the time step from seconds into milliseconds
         
-        self.aircraft.update(t_step, self.bounds)
+        self.aircraft.update(t_step, self.bounds) # Update all the aircraft
         
-        self.log()
+        self.log() # Log the new states
         
     def log(self):
-        state = list(self.aircraft)
-        self.state_log.append(state)
+        """Log the aircraft states
+        """
+        state = list(self.aircraft) # The environment states is over all aircraft
+        self.state_log.append(state) # append this log to the state_log
     
                     
 
 
 class Environment:
+    """This class holds and runs the environment. It is designed to be executed from a child thread from the UI.
+    """
     def __init__(self, bounds:(float,float), grid_centre,sim_t:float = 0.0):
-        self.running = False
-        self.grid_centre = grid_centre
+        self.running = False # A flag for if the sim is running
+        self.grid_centre = grid_centre # The centre coordinates for the middle hex
         
-        self.__state = State(bounds, sim_t)
+        self.__state = State(bounds, sim_t) # Instantiate the state (aircraft)
         
-        self.towers = self.gen_towers()
+        self.towers = self.gen_towers() # Generate the towers (generated in a spiral from the centre.)
         
         
-    @property
+    @property # The sim time formatted in Hours:Minutes:Seconds.miliseconds
     def sim_time(self):
         return (datetime(1,1,1)+timedelta(milliseconds=self.__state.sim_t)).strftime("%H:%M:%S.%f")[:-4]
     
-    @property
+    @property # A formatted printout of the state of the aircraft
     def state(self):
         return self.__state.aircraft
     
-    def gen_towers(self, rings=3, size=150,max_signal=[10]):
-        towers = ring_to(self.grid_centre, rings, size, max_signal)
+    def gen_towers(self, rings=3, size=150,max_signal=[10], random_out:int = 0):
+        """Generate towers in rings
 
-        # for tower in towers:
-        #     if np.random.rand() <= 0.3:
-        #         tower.active = not tower.active
+        Args:
+            rings (int, optional): Number of rings to generate. Defaults to 3.
+            size (int, optional): How wide the towers are. Defaults to 150.
+            max_signal (list, optional): Default starting max signal. Defaults to [10].
+            random_out (int, optional): How many towers to initilise as disconnected. 0 is no towers n>0 is the number of towers to drop. -1 is a random number.
+        """
+        towers = ring_to(self.grid_centre, rings, size, max_signal) # Generate towers to
+
+        
+        if random_out > 0: # Set N towers to be inactive
+            out_idxs = np.random.choice(range(len(towers)), size = max(random_out, len(towers)))
+            for idx in out_idxs:
+                towers[idx].acitve = False
+        
+        elif random_out == -1: # Set a random number of towers to be inactive.
+            rand_vals = np.random.rand(len(towers))
+            idxs = np.where(rand_vals <=0.3)[0]
+            for idx in idxs:
+                towers[idx].active = False
 
         return Tower_List(towers)
         
     def run(self, ts=0.1):
-        self.running = True
+        """The main run loop
+
+        Args:
+            ts (float, optional): The time step in seconds. Defaults to 0.1.
+        """
+        self.running = True # Simulation running flag
         
-        while self.running:
+        while self.running: # Run while the flag is active
             
-            self.__state.update(ts)
-            self.towers.update_towers(self.__state.aircraft)
+            self.__state.update(ts) # Update the aircraft environment
+            self.towers.update_towers(self.__state.aircraft) # Update the tower environment
             
-            time.sleep(ts)
+            time.sleep(ts if ts <=1 else 0.01) # Force a sleep pause
             
             if not any(ac[-1] for ac in self.state):
-                self.running = False
+                self.running = False # Exit the simulation when no aircraft are active
          
     
     def stop(self):
+        """Manual stopping of the simulation (for UI version)
+        """
         self.running = False
         
         return np.array(self.__state.state_log)
