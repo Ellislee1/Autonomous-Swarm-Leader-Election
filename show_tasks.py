96a6d10416195d8aa329116223de5829439c5202
Ellis Thompson <>
2023-09-12 12:31:09 +0100

    labelled graph

--- show_tasks.py
+++ show_tasks.py
@@ -5,8 +5,14 @@ from tqdm import tqdm
 
 heatmap = 'out/heatmap/'
 no_heatmap = 'out/no_heatmap/'
+heatmap_p7 = 'out/heatmap_p7/'
+no_heatmap_p7 = 'out/no_heatmap_p7/'
+heatmap_active_p7 = 'out/heatmap_active_only_p7/'
+heatmap_active = 'out/heatmap_active_only/'
+colours = ['b','r', 'c', 'm', 'b:', 'c:']
 
-spaces = [heatmap,no_heatmap]
+spaces = [heatmap,no_heatmap, heatmap_p7, no_heatmap_p7, heatmap_active,heatmap_active_p7]
+labels = ['heatmap - filtered', 'no heatmap', 'heatmap - filtered 0 Start','no heatmap 0 Start', 'heatmap - unfiltered', 'heatmap - unfiltered 0 Start']
 
 means = []
 for space in spaces:
@@ -16,7 +22,12 @@ for space in spaces:
     max_len = 0
     for file in tqdm(task_space):
         data = np.load(str(file), allow_pickle=True)
-        n_complete.append((data[:,1]-data[:,-1])/data[:,1])
+
+        
+        v = (data[:,1]-data[:,-1])/(data[:,1]+1e-10)
+        v[np.where(data[:,1]==0)[0]] = 0
+        
+        n_complete.append(v)
         max_len = max(max_len, len(data[:,1]))
 
     for i in range(len(n_complete)):
@@ -28,15 +39,18 @@ for space in spaces:
 
         n_complete[i] = np.append(n_complete[i], [n_complete[i][-1]]*to_pad)
 
-    for run in n_complete:
-        plt.plot(run)
+    # for run in n_complete:
+    #     plt.plot(run)
     
-    means.append(np.median(n_complete,axis=0))
-    plt.show()
+    means.append(np.mean(n_complete,axis=0))
+    # plt.show()
 
 for i,m in enumerate(means):
-    plt.plot(m,label='heatmap' if i == 0 else 'random')
+    len_ = np.array(list(range(len(m))))*(1/30)
+    plt.plot(len_,m,colours[i],label=labels[i])
 
+plt.xlabel('Sim Time (s)')
+plt.ylabel('Complete Tasks/ Remaining (Avg.)')
 plt.legend()
 plt.show()
 
